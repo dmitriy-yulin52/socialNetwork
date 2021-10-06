d75487db8e97513b71241f1deda4180dbd4734d2
dmitriy <yulin_dimon@mail.ru>
2021-10-06 15:17:16 +0300

    Made the App component a class component

--- src/Components/app/App.tsx
+++ src/Components/app/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
 import NavBar from "../NavBar/NavBar"
-import {Redirect, Route, withRouter} from "react-router-dom"
+import {Route} from "react-router-dom"
 import UsersContainer from "../Users/Users.container";
 import {HeaderContainer} from '../Header/HeaderContainer';
 import Music from "../Music/Music";
@@ -14,48 +14,65 @@ import {ErrorSnackBar} from "../SnackBar/ErrorSnackBar";
 import {Footer} from "../Footer/Footer";
 import {LoginContainer} from "../Login/LoginContainer";
 import {LinearProgress} from "@material-ui/core";
-import {useSelector} from "react-redux";
-import {selectStateUsersPage} from "../../Redux/selectors";
+import {connect} from "react-redux";
+import {AppStateType} from "../../Redux/reduxStore";
+import {getAuthUserDataThunkCreator} from "../../Redux/auth-reducer";
 
 
 
-export const App = () => {
-    const {
-        isFetching,
-    } = useSelector(selectStateUsersPage)
 
-    return (
-        <div>
-            <div className="app-wrapper">
-                <ErrorSnackBar/>
-                <HeaderContainer/>
-                <NavBar/>
-                <div className='app-wrapper-content'>
-                    {isFetching ? <LinearProgress /> : null}
-                    <Route path='/dialogs'
-                           render={() =>
-                               <DialogsContainer
-                               />}
-                    />
-                    <Route path='/profile/:userId?'
-                           render={() =>
-                               <ProfileContainer/>
-                           }
-                    />
-                    <Route path='/users'
-                           render={() => <UsersContainer/>}
-                    />
-                    <Route path='/news' render={() => <NewsContainer/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
-                    <Route path='/settings' render={() => <Setting/>}/>
-                    <Route path='/friends' render={() => <FriendsContainer/>}/>
-                    <Route path='/login' render={() => <LoginContainer/>}/>
+
+class App extends React.Component<AppType> {
+
+    componentDidMount() {
+        this.props.getAuthUserDataThunkCreator()
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="app-wrapper">
+                    <ErrorSnackBar/>
+                    <HeaderContainer/>
+                    <NavBar/>
+                    <div className='app-wrapper-content'>
+                        {this.props.isFetching ? <LinearProgress/> : null}
+                        <Route path='/dialogs'
+                               render={() =>
+                                   <DialogsContainer
+                                   />}
+                        />
+                        <Route path='/profile/:userId?'
+                               render={() =>
+                                   <ProfileContainer/>
+                               }
+                        />
+                        <Route path='/users'
+                               render={() => <UsersContainer/>}
+                        />
+                        <Route path='/news' render={() => <NewsContainer/>}/>
+                        <Route path='/music' render={() => <Music/>}/>
+                        <Route path='/settings' render={() => <Setting/>}/>
+                        <Route path='/friends' render={() => <FriendsContainer/>}/>
+                        <Route path='/login' render={() => <LoginContainer/>}/>
+                    </div>
+
                 </div>
-
+                <Footer/>
             </div>
-            <Footer/>
-        </div>
-    );
+        );
+    }
 }
 
+//types class component
+type AppType = {
+    isFetching:boolean
+    getAuthUserDataThunkCreator:()=>void
+}
 
+const mapStateToProps = (state:AppStateType)=> {
+    return {
+        isFetching: state.usersPage.isFetching
+    }
+}
+export default connect(mapStateToProps,{getAuthUserDataThunkCreator})(App)
